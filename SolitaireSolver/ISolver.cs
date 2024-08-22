1dd7a99f13bd6e334cd3768e0cd538b365974ee3
Sam Hill <soshjam@soshjam.com>
2024-08-22 12:33:05 -0600

    Create ISolver interface

---
+++ SolitaireSolver/ISolver.cs
@@ -0,0 +1,12 @@
+﻿namespace SolitaireSolver
+{
+    public interface ISolver
+    {
+        /// <summary>
+        /// Calculates the next move in a game of Solitaire.
+        /// </summary>
+        /// <param name="game">The current game.</param>
+        /// <returns>The next move, formatted as a string.</returns>
+        public string CalculateNextMove(ISolitaire game);
+    }
+}
