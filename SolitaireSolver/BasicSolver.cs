680be3cb3bf4382e2825095ffd88562fb08c841f
Sam Hill <soshjam@soshjam.com>
2024-08-22 15:37:27 -0600

    3 more rules

--- SolitaireSolver/BasicSolver.cs
+++ SolitaireSolver/BasicSolver.cs
@@ -11,6 +11,9 @@
             // The things we will try, in order from most to least important.
             Func<string>[] checks = {
                 tryAddToFoundationPilesFromBoard,
+                tryAddToFoundationPilesFromStock,
+                tryMoveKing,
+                tryMoveAndReveal,
             };
 
             SolverState lastState = state;
@@ -57,7 +60,7 @@
         // Step Methods
         // These return a command if the move is possible, or "NO" if not.
 
-        // First: Try to add to a foundation pile from something on the board
+        // 01: Try to add to a foundation pile from something on the board - O(N)
         protected string tryAddToFoundationPilesFromBoard()
         {
             char[] tops = getPileTops();
@@ -70,7 +73,7 @@
                 if (Card.GetValue(tops[i]) == minimumFoundationValue + 1)
                     canAdd[i] = true;
 
-            // Prioritize the pile with the most face-down cards
+            // Find the pile with the most face-down cards
             int mostFaceDownCards = 0;
             for (int i = 0; i < 7; i++)
                 if (canAdd[i] && facedown[i] > mostFaceDownCards)
@@ -83,6 +86,121 @@
 
             return "NO";
         }
+
+        // 01: Try to add to a foundation pile from the stock - O(1)
+        protected string tryAddToFoundationPilesFromStock()
+        {
+            int minimumFoundationValue = getMinimumFoundationValue();
+
+            // We can add if this is 1 above the minimum
+            if (Card.GetValue(stock) == minimumFoundationValue + 1)
+                return "stf";
+
+            return "NO";
+        }
+
+        // 02: Move a king to an empty column, if its has face-down cards in its column
+        protected string tryMoveKing()
+        {
+            char[] bottoms = getPileBottoms();
+            List<int> needsMoving = new List<int>(); // columns with kings that need moving
+            List<int> emptyColumns = new List<int>();
+
+            for (int i = 0; i < 7; i++)
+            {
+                // Column is empty
+                if (board[i].Count == 0)
+                {
+                    emptyColumns.Add(i);
+                    continue;
+                }
+
+                // King atop a face-down card
+                if (Card.GetValue(bottoms[i]) == 13 && board[i][0] == '#')
+                {
+                    needsMoving.Add(i);
+                    continue;
+                }
+            }
+
+            // If we can move something, do it
+            if (needsMoving.Count > 0 && emptyColumns.Count > 0)
+            {
+                int targetColumn = emptyColumns.Min();
+                int[] facedown = getFaceDownCardsOnBoard();
+
+                // Find the pile with the most face-down cards
+                int mostFaceDownCards = 0;
+                for (int i = 0; i < 7; i++)
+                    if (needsMoving.Contains(i) && facedown[i] > mostFaceDownCards)
+                        mostFaceDownCards = facedown[i];
+
+                for (int i = 0; i < 7; i++)
+                    if (needsMoving.Contains(i) && facedown[i] == mostFaceDownCards)
+                        return "move " + i + " " + targetColumn;
+            }
+
+            return "NO";
+        }
+
+        // 03: Move a stack on the board to reveal a face-down card - O(N) I think
+        protected string tryMoveAndReveal()
+        {
+            char[] bottoms = getPileBottoms();
+            char[] tops = getPileTops();
+            int[] facedown = getFaceDownCardsOnBoard();
+            bool[] canMove = new bool[7];
+
+            // Loop through bottoms, see if they have a matching top
+            for (int i = 0; i < 7; i++)
+            {
+                for (int j = 0; j < 7; j++)
+                {
+                    // Skip the same column
+                    if (j == i) continue;
+
+                    // Skip matching colors
+                    if (Card.IsBlack(bottoms[i]) == Card.IsBlack(tops[j])) continue;
+
+                    // Skip mismatched values
+                    if (Card.GetValue(bottoms[i]) != Card.GetValue(tops[j]) - 1) continue;
+
+                    // We can move this stack
+                    canMove[i] = true;
+                }
+            }
+
+            // Find the pile with the most face-down cards
+            int mostFaceDownCards = 0;
+            for (int i = 0; i < 7; i++)
+                if (canMove[i] && facedown[i] > mostFaceDownCards)
+                    mostFaceDownCards = facedown[i];
+
+            // Move the rightmost column with the most face-down cards
+            for (int i = 6; i >= 0; i--)
+            {
+                if (canMove[i] && facedown[i] == mostFaceDownCards)
+                {
+                    // Loop through to find the right column again
+                    for (int j = 0; j < 7; j++)
+                    {
+                        // Skip the same column
+                        if (j == i) continue;
+
+                        // Skip matching colors
+                        if (Card.IsBlack(bottoms[i]) == Card.IsBlack(tops[j])) continue;
+
+                        // Skip mismatched values
+                        if (Card.GetValue(bottoms[i]) != Card.GetValue(tops[j]) - 1) continue;
+
+                        return "move " + i + " " + j;
+                    }
+                }
+            }
+
+            return "NO";
+        }
+
 
         // Helper Methods
 
@@ -107,10 +225,20 @@
         {
             char[] bottoms = new char[7];
             for (int i = 0; i < 7; i++)
+            {
                 if (board[i].Count > 0)
-                    bottoms[i] = board[i].First();
+                {
+                    foreach (char c in board[i])
+                    {
+                        if (bottoms[i] == '\0' && c != '#')
+                            bottoms[i] = c;
+                    }
+                }
                 else
+                {
                     bottoms[i] = '\0';
+                }
+            }
             return bottoms;
         }
 
