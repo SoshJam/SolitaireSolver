cdbf1eb7b3a684803b94f29122d8ff1056f78329
Sam Hill <soshjam@soshjam.com>
2024-08-22 10:12:17 -0600

    Finished implementation of BasicSolitaireGame

--- SolitaireSolver/BasicSolitaireGame.cs
+++ SolitaireSolver/BasicSolitaireGame.cs
@@ -47,7 +47,38 @@ namespace SolitaireSolver
 
         public bool BoardToFoundations(int column, out char revealed)
         {
-            throw new NotImplementedException();
+            // If the target column is empty, this is invalid.
+            if (board[column].Count == 0)
+            {
+                revealed = '\0';
+                return false;
+            }
+
+            char card = board[column].Last();
+            int suit = (int) Card.GetSuit(card);
+
+            // The card can be placed there if:
+            // a) It is 1 greater than the card of the same suit, OR
+            // b) It is an Ace (value 1) and the pile of that suit is empty.
+            if (card - foundationPiles[suit] != 1 && !(foundationPiles[suit] == '\0' && Card.GetValue(card) == 1))
+            {
+                revealed = '\0';
+                return false;
+            }
+
+            // Update the pile
+            foundationPiles[suit] = card;
+            board[column].RemoveAt(board[column].Count - 1);
+
+            // Reveal the top card of this pile if necessary
+            revealed = '\0';
+            if (board[column].Count == 0)
+            {
+                revealed = deck.Pop();
+                board[column].Add(revealed);
+            }
+
+            return true;
         }
 
         public char CycleStock()
@@ -75,7 +106,47 @@ namespace SolitaireSolver
 
         public bool FoundationsToBoard(Suit suit, int target)
         {
-            throw new NotImplementedException();
+            char card = foundationPiles[(int) suit];
+
+            List<char> targetColumn = board[target];
+
+            // If the target column is empty, the top card must be a king to move it there
+            if (targetColumn.Count == 0)
+            {
+                if (Card.GetValue(card) != 13)
+                    return false;
+
+                // Otherwise the move is valid
+                targetColumn.Add(card);
+
+                // Decrement the foundation pile by 1, or set it to empty if the moved card is an Ace
+                if (Card.GetValue(card) > 1)
+                    foundationPiles[(int)suit]--;
+                else
+                    foundationPiles[(int)suit] = '\0';
+
+                return true;
+            }
+
+            // Get the top card of this column
+            char targetCard = targetColumn.Last();
+
+            // The move is valid if they are of different colors and topCard's value is one less than that of targetCard
+            if (Card.IsBlack(targetCard) != Card.IsBlack(card) && Card.GetValue(targetCard) - 1 == Card.GetValue(card))
+            {
+                targetColumn.Add(card);
+
+                // Decrement the foundation pile by 1, or set it to empty if the moved card is an Ace
+                if (Card.GetValue(card) > 1)
+                    foundationPiles[(int)suit]--;
+                else
+                    foundationPiles[(int)suit] = '\0';
+
+                return true;
+            }
+
+            // Otherwise, it's invalid.
+            return false;
         }
 
         public List<char>[] GetBoard()
@@ -101,7 +172,76 @@ namespace SolitaireSolver
 
         public bool MovePile(int start, int end, out char revealed, int offset = 0)
         {
-            throw new NotImplementedException();
+            // Ensure there are enough cards
+            if (board[start].Count <= offset)
+            {
+                revealed = '\0';
+                return false;
+            }
+
+            char baseCard = board[start][offset];
+            List<char> movedCards = new List<char>();
+            for (int i = offset; i < board[start].Count; i++)
+            {
+                movedCards.Add(i);
+            }
+
+            // If the target column is empty, the base card must be a king to move it there
+            if (board[end].Count == 0)
+            {
+                if (Card.GetValue(baseCard) != 13)
+                {
+                    revealed = '\0';
+                    return false;
+                }
+
+                // Otherwise the move is valid
+
+                // Move cards one at a time
+                foreach (char c in movedCards)
+                {
+                    board[start].Add(c);
+                    board[start].Remove(c);
+                }
+
+                // Reveal a card if necessary
+                revealed = '\0';
+                if (offset == 0 || board[start].Count == 0) // Second half of this should be redundant, but just in case
+                {
+                    revealed = deck.Pop();
+                    board[start].Add(revealed);
+                }
+
+                return true;
+            }
+
+            // Get the top card of this column
+            char targetCard = board[end].Last();
+
+            // The move is valid if they are of different colors and topCard's value is one less than that of targetCard
+            if (Card.IsBlack(targetCard) != Card.IsBlack(baseCard) && Card.GetValue(targetCard) - 1 == Card.GetValue(baseCard))
+            {
+                // Move cards one at a time
+                foreach (char c in movedCards)
+                {
+                    board[start].Add(c);
+                    board[start].Remove(c);
+                }
+
+                // Reveal a card if necessary
+                revealed = '\0';
+                if (offset == 0 || board[start].Count == 0) // Second half of this should be redundant, but just in case
+                {
+                    revealed = deck.Pop();
+                    board[start].Add(revealed);
+                }
+
+                return true;
+            }
+
+            // Otherwise, it's invalid.
+            revealed = '\0';
+            return false;
         }
 
         public char PeekStock()
@@ -120,12 +260,20 @@ namespace SolitaireSolver
             foreach (char c in shuffledCards)
                 deck.Push(c);
 
+            Stack<char> tempDeck = new Stack<char>();
+
             // Draw 7 cards for the board
             for (int i = 0; i < 7; i++)
             {
                 board[i].Clear(); // Clear this column
                 board[i].Add(deck.Pop()); // Add one card
                 faceDownBoardCards[i] = i; // Reset the amount of face-down cards
+
+                // Add face-down cards to this to ensure cards are drawn in the correct order
+                for (int j = 0; j < i; j++)
+                {
+                    tempDeck.Push(deck.Pop());
+                }
             }
 
             // Draw 24 cards for the stock
@@ -135,6 +283,8 @@ namespace SolitaireSolver
                 stockPile.Push(deck.Pop());
 
             // There should be 21 cards left in the deck representing the face-down cards on the board.
+            while (tempDeck.Count > 0)
+                deck.Push(tempDeck.Pop());
 
             // Reset the foundation piles.
             for (int i = 0; i < 3; i++)
@@ -143,7 +293,47 @@ namespace SolitaireSolver
 
         public bool StockToBoard(int target, out char revealed)
         {
-            throw new NotImplementedException();
+            // Check the suit of the card atop the waste pile
+            char topCard = PeekStock();
+
+            // If it's a null character the waste pile is empty.
+            if (topCard == '\0')
+            {
+                revealed = '\0';
+                return false;
+            }
+
+            List<char> targetColumn = board[target];
+
+            // If the target column is empty, the top card must be a king to move it there
+            if (targetColumn.Count == 0)
+            {
+                if (Card.GetValue(topCard) != 13)
+                {
+                    revealed = '\0';
+                    return false;
+                }
+
+                // Otherwise the move is valid
+                targetColumn.Add(topCard);
+                revealed = PeekStock();
+                return true;
+            }
+
+            // Get the top card of this column
+            char targetCard = targetColumn.Last();
+
+            // The move is valid if they are of different colors and topCard's value is one less than that of targetCard
+            if (Card.IsBlack(targetCard) != Card.IsBlack(topCard) && Card.GetValue(targetCard) - 1 == Card.GetValue(topCard))
+            {
+                targetColumn.Add(topCard);
+                revealed = PeekStock();
+                return true;
+            }
+
+            // Otherwise, it's invalid.
+            revealed = '\0';
+            return false;
         }
 
         public bool StockToFoundations(out char revealed)
